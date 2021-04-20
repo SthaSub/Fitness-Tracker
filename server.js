c54ef0fc115af569f7c21b8701d8aa42b58da168
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-04-20 14:31:30 +0930

    deploying

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
   useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
